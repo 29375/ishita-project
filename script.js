0c6c49ccad75f2c66eef048790acf8e3fa62fb34
Ishita <85047951+29375@users.noreply.github.com>
2023-07-15 14:33:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,14 +36,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "Developer", "coder", "Designer", "Freelancer"],
+        strings: ["Student", "Developer", "coder", "Problem Solver"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [ "Student","Developer","coder", "Designer", "Freelancer"],
+        strings: [ "Student","Developer","coder", "Problem Solver"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
